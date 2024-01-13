b9d845c575b9a7a0e7ade0ca86e642c1a905a57c
MaksimkaKrul <111635609+MaksimkaKrul@users.noreply.github.com>
2024-01-13 17:47:27 +0200

    Update words.js
    single line if

--- Tasks/words.js
+++ Tasks/words.js
@@ -3,11 +3,12 @@
 //The flag thing is still here and will fix in next commit
 //Deleted all of the flags, and instead of it just count with .split
 //Deleted stupid \n after if statement 
+//Now if(inputString =='') is single line
+
 'use strict'
 const countWordsInArray = (inputString) => {
-  if (inputString == ''){
-    return 0
-  }
+  if (inputString == '') return 0;
+  
   return inputString.split(' ').length;
 };
 
