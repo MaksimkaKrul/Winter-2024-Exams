837d58e6903d08d2d443f19347b4cddfff4b6aba
MaksimkaKrul <111635609+MaksimkaKrul@users.noreply.github.com>
2024-01-13 17:40:16 +0200

    Update month.js
    yeah, looks better and works better with .includes.

--- Tasks/month.js
+++ Tasks/month.js
@@ -19,7 +19,7 @@ const getMonthIndex = (monthName) => {
   let lowerMonthName = monthName.toLowerCase();
 
   for (const [index, month] of monthList.entries()) {
-    if (lowerMonthName.indexOf(month) == 0) return index + 1;
+    if (lowerMonthName.includes(month) return index + 1;
   }
 
   return -1;
