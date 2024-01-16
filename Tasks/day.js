2a2702126c458a3fe50121034e5174ecd2caefdd
MaksimkaKrul <111635609+MaksimkaKrul@users.noreply.github.com>
2024-01-16 20:09:34 +0200

    Update day.js
    added const and now it out of the fuction

--- Tasks/day.js
+++ Tasks/day.js
@@ -8,8 +8,9 @@
 
 'use strict'
 
+const dayNameList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];  
+
 const getDayIndex = (dayName) => {
-  let dayNameList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];  
 
   for (const [index, day] of dayNameList.entries()) {
     if (dayName.indexOf(day.toLowerCase()) === 0) return index + 1;
